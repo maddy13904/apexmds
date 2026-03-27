5d02936227780da82f686e1cce7951b6db6e153d
maddy13904 <maddy13904@gmail.com>
2026-03-27 18:57:05 +0530

    App logo updated and App.json changes

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  server: {
+    host: "0.0.0.0",
+    port: 8100 // 🔥 change to any port you want
+  }
 })
